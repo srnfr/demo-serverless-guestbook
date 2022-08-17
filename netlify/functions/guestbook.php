6c0048b794b8915dff505fa35e73c41f5ee66525
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-08-17 13:49:14 +0200

    Create guestbook.php

---
+++ netlify/functions/guestbook.php
@@ -0,0 +1,45 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'vendor/predis/predis/autoload.php';
+
+Predis\Autoloader::register();
+
+if (isset($_GET['cmd']) === true) {
+  $host = 'redis-master';
+  if (getenv('GET_HOSTS_FROM') == 'env') {
+    $host = getenv('REDIS_LEADER_SERVICE_HOST');
+    $port = getenv('REDIS_LEADER_SERVICE_PORT');
+  }
+  header('Content-Type: application/json');
+  if ($_GET['cmd'] == 'set') {
+    $client = new Predis\Client([
+      'scheme' => 'tcp',
+      'host'   => $host,
+      'port'   => $port,
+      'password'   => getenv('REDIS_PASSWORD'),
+    ]);
+
+    $client->set($_GET['key'], $_GET['value']);
+    print('{"message": "Updated"}');
+  } else {
+    $host = 'redis-replica';
+    if (getenv('GET_HOSTS_FROM') == 'env') {
+      $host = getenv('REDIS_LEADER_SERVICE_HOST');
+      $port = getenv('REDIS_LEADER_SERVICE_PORT');
+    }
+    $client = new Predis\Client([
+      'scheme' => 'tcp',
+      'host'   => $host,
+      'port'   => $port,
+      'password'   => getenv('REDIS_PASSWORD'),
+    ]);
+
+    $value = $client->get($_GET['key']);
+    print('{"data": "' . $value . '"}');
+  }
+} else {
+  phpinfo();
+} ?>
